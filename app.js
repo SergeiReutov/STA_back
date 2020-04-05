986edb44912ae8b0fb438a4d30def5900d79e5fe
Sergei Reutov <basic.mehmat@gmail.com>
2020-04-05 16:23:26 +0200

    Yahoo finance added

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const yahooFinance = require('yahoo-finance');
 
 const app = new Koa();
 const router = new Router();
 
-router.post('/stock/:id', (ctx) => {
-  const id = ctx.params.id;
-  const params = ctx.request.req;
-  ctx.body = {
-    id
-  };
+router.get('/stock/:symbol', async (ctx) => {
+  await yahooFinance.historical({
+    symbol: ctx.params.symbol,
+    from: '2020-01-07',
+    to: '2020-01-09',
+    period: 'd'
+  }, function (err, quotes) {
+    ctx.body = {
+      err,
+      quotes
+    };
+  });
 });
 
 app.use(router.routes()).use(router.allowedMethods());
