37827dc123ebd9bc29e82d2594ebb1cda3241c4c
Sergei Reutov <basic.mehmat@gmail.com>
2020-04-13 17:45:06 +0200

    Koa CORS

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const Koa = require('koa');
+const cors = require('@koa/cors');
 const Router = require('koa-router');
 const yahooFinance = require('yahoo-finance');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/stock/:symbol', async (ctx) => {
+router.get('/api/chartData/:symbol', async (ctx) => {
   await yahooFinance.historical({
     symbol: ctx.params.symbol,
     from: '2020-01-07',
     to: '2020-01-09',
     period: 'd'
   }, function (err, quotes) {
-    ctx.body = {
-      err,
-      quotes
-    };
+    console.log(err);
+    console.log(quotes);
+    ctx.body = quotes;
+    return ctx;
   });
 });
 
+app.use(cors());
+
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000);
+app.listen(3001);
